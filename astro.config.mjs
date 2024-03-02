d82fcbff2305fc0983582d2c4512295e215f4802
erasoftcr <161968066+erasoftcr@users.noreply.github.com>
2024-03-02 19:27:03 +0000

    Config Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: vercel(),
   integrations: [tailwind(), compress(),]
 });
